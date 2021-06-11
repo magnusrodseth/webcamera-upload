463139df6e5f09ab1aa4c9a00d1fd1a1727c8688
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-06-11 15:25:26 +0200

    Add upload.py and a thorough README

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-webcamera-upload
+# Webcamera upload 📷
+
+## What is it ❓
+
+This repository has a single Python script. `upload.py` allows you to securely transmit a file from your local computer to a destination host using the [SFTP protocol](https://www.digitalocean.com/community/tutorials/how-to-use-sftp-to-securely-transfer-files-with-a-remote-server).
+
+This repository uses the `upload.py` script to transmit webcamera images from a client computer to a remote server.
+
+## Developer Information 🙋🏼‍♂️
+
+Developed by Magnus Rødseth and Julian Grande, summer 2021.
+
+## Setting environment variables ❗️
+
+A very important prerequisite in order for `upload.py` to work is setting correct environment variables before running the script. Start by creating a file called `.env` in the repository directory (`webcamera-upload`).
+
+**`webcamera-upload/.env`**
+
+```env
+HOSTNAME=destination_ip_address
+USERNAME=destination_username
+PASSWORD=destination_password
+```
+
+As you can see from the sample file above, this script requires you to know the listed information about your destination host.
+
+## Freezing requirements ❄️
+
+**`Terminal`**
+
+```shell
+python3 -m pip freeze > requirements.txt
+```
+
+## Virtual environment 🌳
+
+**`Terminal`**
+
+```shell
+# Create virtual environment
+python3 -m venv venv
+
+# Activate using Windows:
+venv\Scripts\activate.bat
+
+# OR
+
+# Activate using Mac or Unix:
+source venv/bin/activate
+
+# Install requirements
+python3 -m pip install -r requirements.txt
+```
---
+++ upload.py
@@ -0,0 +1,53 @@
+# Import modules
+import pysftp
+import sys
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+
+def upload(argv):
+    """
+    This method uses SFTP to upload a given file to a given host.
+    The hostname, destination username and destination password are set
+    in a .env file in the same directory as this upload.py file.
+
+    :param argv: Variable length arguments. The first argument corresponds to the local file path. The second argument corresponds to the destination directory on the destination host.
+
+    :return: a boolean value determining whether the operation was successful or not.
+    """
+
+    # argv should only process 2 arguments
+    if len(argv) != 2:
+        print("> Invalid input. Please use the following format:")
+        print(f'$ python3 upload.py local_file_path destination_directory')
+        return False
+
+    local_file_path = argv[0]
+    destination_directory = argv[1]
+
+    print(f'> Local file path: {local_file_path}')
+    print(f'> Destination directory: {destination_directory}')
+
+    with pysftp.Connection(
+            host=os.environ.get('HOSTNAME'),
+            username=os.environ.get('USERNAME'),
+            password=os.environ.get('PASSWORD')
+    ) as sftp:
+        with sftp.cd(destination_directory):
+            sftp.put(local_file_path)  # Upload file
+
+            return True
+
+
+if __name__ == '__main__':
+    # Syntax: python3 upload.py local_file_path destination_directory
+    succeeded = upload(sys.argv[1:])
+
+    print(
+        "> Successfully uploaded file!"
+        if succeeded
+        else "> An error occurred when trying to upload the file!"
+    )
